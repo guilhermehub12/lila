1ef77e30aa037e593a84704c70e76880d3af367d
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2024-12-04 00:11:34 -0300

    feat: create main page structure with component sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
-import Header from "./components/Header";
-import Layout from "./layout";
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Contact from "./components/Contact";
+import Products from "./components/Products";
+import Testimonials from "./components/Testimonials";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import FAQ from "./components/FAQ";
+import Services from "./components/Services";
+import WhyUs from "./components/WhyUs";
 
 export default function Home() {
   return (
-    <Layout>
-      <Header />
-    </Layout>
+    <main>
+      <Navbar />
+      <Hero />
+      <About />
+      <WhyUs />
+      <Services />
+      <Products />
+      <Testimonials />
+      <FAQ />
+      <Contact />
+      <Footer />
+    </main>
   )
 }
